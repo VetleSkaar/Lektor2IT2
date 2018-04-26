6495cd72334b8cd45a5184116888b55fec44e190
VetleSkaar <33469708+VetleSkaar@users.noreply.github.com>
2018-04-26 13:16:46 +0200

    Endret til ny kode med funksjonell navngivning

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 var Results = [
-  ["Fullt Navn", "Klasse", "Epost", "Adresse"],
-  ["", 50, 100, 500],
+  ["Col1", "Col2", "Col3", "Col4"],
+  ["Data", 50, 100, 500],
   ["Data", -100, 20, 100],
 ];
 
 exportToCsv = function() {
-  var CsvString = '"sep=,"';
+  var CsvString = "";
   Results.forEach(function(RowItem, RowIndex) {
     RowItem.forEach(function(ColItem, ColIndex) {
       CsvString += ColItem + ',';
     });
     CsvString += "\r\n";
   });
-  window.open('data:application/csv,' + encodeURIComponent(CsvString));
+  CsvString = "data:application/csv," + encodeURIComponent(CsvString);
+ var x = document.createElement("A");
+ x.setAttribute("href", CsvString );
+ x.setAttribute("download","somedata.csv");
+ document.body.appendChild(x);
+ x.click();
 }

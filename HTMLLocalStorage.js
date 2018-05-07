c424af6ee786e1d8c23559fa1c9fdbf3cd9398ea
VetleSkaar <33469708+VetleSkaar@users.noreply.github.com>
2018-05-07 10:18:20 +0200

    Update HTMLLocalStorage.js
    La til eksempel

--- HTMLLocalStorage.js
+++ HTMLLocalStorage.js
@@ -4,3 +4,12 @@ Storage.prototype.setObj = function(key, obj){
 Storage.prototype.getObj = function(key){
     return JSON.parse(this.getItem(key))
 }
+
+//Eksempel
+var array = [1,2,3,4,5,,6,7,8,9];
+
+localStorage.setObj("arr", array);
+
+var recallArr = localstorage.getObj("arr");
+
+//array === recallArr
